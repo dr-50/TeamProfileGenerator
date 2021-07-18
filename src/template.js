a6e88360860ae87975abd943f0c6387940bc57b7
Devon <devonross50@yahoo.com>
2021-07-18 18:52:15 -0500

    html template and styling

--- src/template.js
+++ src/template.js
@@ -1,40 +1,79 @@
-const generateTeam = team => {
-    
-    const generateManager = manager => {
-        return `
-        <div class="card employee-card">
-        <div class="card-header">
-            <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-            </ul>
-        </div>
-    </div>
-        `;
-    };
-
-    const html = [];
-    html.push(team.filter(employee => employee.getRole() === "Manager")
-        .map(manager => generateManager(manager))
-        )
-        return html.join("")
-}
-
-module.exports = team => {
+const generateTeam = (team) => {
+  const generateManager = (manager) => {
     return `
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Name: ${manager.getName()}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Role: ${manager.getRole()}</h6>
+            <p class="card-text">Id: ${manager.getId()}</p>
+            <p class="card-text">Email: ${manager.getEmail()}</p>
+            <p class="card-text">Office Phone: ${manager.getOfficeNumber()}</p>
+        </div>
+        </div>
+        `;
+  };
+
+  const generateIntern = (intern) => {
+      return `
+      <div class="card" style="width: 18rem;">
+      <div class="card-body">
+          <h5 class="card-title">Name: ${intern.getName()}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Role: ${intern.getRole()}</h6>
+          <p class="card-text">Id: ${intern.getId()}</p>
+          <p class="card-text">Email: ${intern.getEmail()}</p>
+          <p class="card-text">School: ${intern.getSchool()}</p>
+      </div>
+      </div>
+      `;
+  }
+
+  const generateEngineer = (engineer) => {
+      return `
+      <div class="card" style="width: 18rem;">
+      <div class="card-body">
+          <h5 class="card-title">Name: ${engineer.getName()}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Role: ${engineer.getRole()}</h6>
+          <p class="card-text">Id: ${engineer.getId()}</p>
+          <p class="card-text">Email: ${engineer.getEmail()}</p>
+          <p class="card-text">School: ${engineer.getGithub()}</p>
+      </div>
+      </div>
+      `
+  }
+
+
+
+  const html = [];
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => generateManager(manager))
+  );
+  html.push(
+      team
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => generateIntern(intern))
+  )
+  html.push(
+    team
+    .filter((employee) => employee.getRole() === "Engineer")
+    .map((engineer) => generateEngineer(engineer))
+)
+  return html.join("");
+};
+
+module.exports = (team) => {
+  return `
     <!DOCTYPE html>
     <html lang="en">
     
     <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" />
     </head>
     
     <body>
     ${generateTeam(team)}
     </body>
-    </html>`
-}
+    </html>`;
+};
